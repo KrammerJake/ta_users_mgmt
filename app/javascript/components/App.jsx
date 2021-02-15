93ba12015ff7d2e281b0d70d8a2676250e577cd0
Jake Krammer <jake.krammer1@gmail.com>
2021-02-14 22:03:12 -0700

    Add a simple index page that renders a React app

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div style={{ display: "grid", placeItems: "center", height: "100vh" }}>
+      <h1>Hello React!</h1>
+    </div>
+  );
+};
